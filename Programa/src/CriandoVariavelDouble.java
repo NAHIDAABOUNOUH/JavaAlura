95247cd02b405c16e4d3cb5689190d237cc6a072
NAHIDAABOUNOUH <nahida.noroni@gmail.com>
2022-03-16 18:50:07 -0300

    Meus projetos

---
+++ Programa/src/CriandoVariavelDouble.java
@@ -0,0 +1,13 @@
+
+public class CriandoVariavelDouble {
+	public static void main(String[] args) {
+		double salario = 1250.70;
+		System.out.println(salario);
+
+		System.out.println("O salário de João é " + salario);
+
+		double divisao = 5.0 / 2;
+		System.out.println(divisao);
+
+	}
+}
